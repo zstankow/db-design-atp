1a8d22ee5bc19c2b8a5f00ead2cab3100ba36dd8
Nathan Szpilman <nathanszpilman@hotmail.com>
2023-12-20 20:07:38 +0200

    players first then tournaments

--- main.py
+++ main.py
@@ -96,8 +96,9 @@ def main():
         execute_sql_file()
         print(f"Database 'tennis' created.")
         print(f"Collecting data... This will take a few minutes...")
-        add_tournament_info()
         add_players_info()
+        add_tournament_info()
+
 
     else:
         print("Invalid command. Supported commands: tournaments, ranking, empty_db, create_db.")
